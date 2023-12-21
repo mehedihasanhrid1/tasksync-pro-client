c568d5d69bbcbe118720428cbf9f885a1da44b95
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2023-12-21 19:18:40 +0600

    Login System Implimented

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -3,6 +3,7 @@ import Root from "../layouts/Root";
 import Errorpage from "../components/Errorpage"
 import Home from "../pages/home/Home";
 import Register from "../pages/accounts/Register";
+import Login from "../pages/accounts/Login";
 
 const routers = createBrowserRouter(
     [
@@ -18,6 +19,10 @@ const routers = createBrowserRouter(
                 {
                     path:'/register',
                     element:<Register/>
+                },
+                {
+                    path:'/login',
+                    element:<Login/>
                 }
             ]
         }
